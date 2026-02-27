1280adad21d5ab803bedb35ed8fc716a004f8b7e
AlmondTeaa <almontejosc@gmail.com>
2026-02-27 16:10:26 +0800

    Style: /recipe design v0

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
 
-router.get("/recipe", (req, res) => {
+router.get("/recipe", async (req, res) => {
     const queriedRecipe = req.query.dish
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${queriedRecipe}`
+    console.log(url);
+    try {
+        const {data} = await axios.get(url)
+        console.log(data);
+    }
+    catch (error) {
+        console.log(error);
+    }
 
     console.log(queriedRecipe);
 }) 
